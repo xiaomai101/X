e65ac7efe78d7c44812bc07c5f45559c384bdc6f
xiaomai101 <144382403+xiaomai101@users.noreply.github.com>
2024-01-10 22:22:45 +0800

    Add files via upload

---
+++ xxboy.js
@@ -0,0 +1,11 @@
+[rewrite_local]
+^https:\/\/mi-eo\.gdt\.qq\.com\/gdt_mview\.fcg\?spsa=1&posid url reject-dict
+^https:\/\/app-v1\.ecoliving168\.com\/api\/v1\/advert\/config\?pack url reject-dict
+^https:\/\/m\.xo2xo\.live\/api\/VersionIosNew url reject-dict
+
+[mitm]
+
+hostname = m.xo2xo.live, app-v1.ecoliving168.com, app-v1.ecoliving168.com
+
+
+**********************************************/
